3bb69d0041f5d4aa248f4d26e27285f456b8837f
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-08-27 16:02:04 -0500

    feat: follow car data

---
+++ src/data/followCar.ts
@@ -0,0 +1,153 @@
+import { GameData } from "../interfaces/Game";
+import getCountryByCode from "../utils/getCountryByCode";
+
+const followCar: GameData[] = [
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_1",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=6.3346781,5.7168018",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_2",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=6.3634211,5.70537",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_3",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=6.5845591,3.3909198",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_4",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=6.4609593,3.6722734",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_5",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=6.6873035,5.2423166",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_6",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=8.9496685,7.1048294",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_7",
+    city: getCountryByCode("NG"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=6.1547565,6.8346172",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_8",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=36.7331432,10.3233181",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_9",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=33.360185,10.4691935",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_10",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=33.8248752,10.8717497",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_11",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=36.8236347,10.0709048",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_12",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=36.7833304,10.0741859",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_13",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=36.7733563,10.0533879",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_14",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=35.6837659,10.0924471",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_15",
+    city: getCountryByCode("TN"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=36.8878563,10.3227061",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_16",
+    city: getCountryByCode("IL"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=32.0100442,35.2614274",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_17",
+    city: getCountryByCode("IL"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=32.2195838,35.1744489",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_18",
+    city: getCountryByCode("IL"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=31.8994134,35.2070464",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_19",
+    city: getCountryByCode("IL"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=32.2302107,34.9448257",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_20",
+    city: getCountryByCode("IL"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=30.7973474,35.2451424",
+  },
+  {
+    image:
+      "https://tbzaiuuumjycdssbuhdy.supabase.co/storage/v1/object/public/images/follow-car/follow_car_21",
+    city: getCountryByCode("PH"),
+    localization:
+      "https://maps.google.com/maps?q=&layer=c&cbll=16.4546652,121.7714355",
+  },
+];
+export default followCar;
