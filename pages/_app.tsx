26bdbcdfc2bd02538beb53ff9fe7bb214e985686
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-08-28 02:27:48 -0500

    feat: creating loading between pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,18 @@ import createEmotionCache from "../src/createEmotionCache";
 import Header from "../src/components/Header";
 import { NextIntlProvider } from "next-intl";
 
+import { Router } from "next/router";
+import NProgress from "nprogress";
+import NProgressStyles from "../src/components/NProgressStyles";
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -32,6 +42,7 @@ export default function MyApp(props: MyAppProps) {
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
+          <NProgressStyles />
 
           <Header />
 
